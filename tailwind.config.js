790136964ebdda236cdb68974ecf57a883cb961e
loqman1235 <tonyvito12@gmail.com>
2024-01-15 07:18:05 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        background: "#200049",
+        primary: "#11EBD2",
+        accent: "#3A0155",
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
