b4a3f2c4ca4f9f0119dc439b4434f1135963264c
loqman1235 <tonyvito12@gmail.com>
2024-01-17 12:11:15 +0100

    commit

--- src/components/common/Reveal.tsx
+++ src/components/common/Reveal.tsx
@@ -1,4 +1,6 @@
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 interface RevealProps {
   children?: React.ReactNode;
@@ -6,16 +8,32 @@ interface RevealProps {
 }
 
 const Reveal: React.FC<RevealProps> = ({ children, width }) => {
+  const controls = useAnimation();
+  const targetRef = useRef<HTMLDivElement>(null);
+  const isInView = useInView(targetRef, { once: true });
+  const variants = {
+    hidden: { opacity: 0, y: 20, scale: 0.95 },
+    visible: { opacity: 1, y: 0, scale: 1 },
+  };
+
+  useEffect(() => {
+    if (isInView) {
+      controls.start("visible");
+    }
+  }, [controls, isInView]);
+
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
+      ref={targetRef}
+      initial="hidden"
+      animate={controls}
+      variants={variants}
+      transition={{ duration: 0.8, ease: [0.6, -0.05, 0.01, 0.99] }}
       className={width}
     >
       {children}
     </motion.div>
   );
 };
+
 export default Reveal;
