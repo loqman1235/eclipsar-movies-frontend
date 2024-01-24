125226e9dca9c3238af2cffae45c958cc1366b24
loqman1235 <tonyvito12@gmail.com>
2024-01-24 12:42:28 +0100

    commit

---
+++ src/components/Main/LeftSidebar/SearchBar.tsx
@@ -0,0 +1,26 @@
+import Svg from "../../../Icons/Svg";
+
+const SearchBar = () => {
+  return (
+    <div className="w-full ">
+      <form className="w-full h-[52px] relative">
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-full h-full border border-white/30 rounded-xl bg-transparent outline-none pl-12"
+        />
+        <button className="absolute left-4 top-1/2 -translate-y-[40%]">
+          <Svg icon="search" size={24} />
+        </button>
+        <button className="absolute right-4 top-1/2 -translate-y-[40%]">
+          <Svg
+            icon="search-settings"
+            size={24}
+            color="var(--secondary-color)"
+          />
+        </button>
+      </form>
+    </div>
+  );
+};
+export default SearchBar;
--- src/components/Main/LeftSidebar/index.tsx
+++ src/components/Main/LeftSidebar/index.tsx
@@ -1,10 +1,12 @@
+import SearchBar from "./SearchBar";
+
 const LeftSidebar = () => {
   return (
     <div
       className="w-right-sidebar h-screen border-l-2
     border-l-white/5 sticky top-0 hidden md:block px-10 py-5"
     >
-      Left Sidebar
+      <SearchBar />
     </div>
   );
 };
