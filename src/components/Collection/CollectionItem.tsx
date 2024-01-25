251906914d9869ed463670718e7596891910b5fc
Tony Vito <76605464+loqman1235@users.noreply.github.com>
2024-01-25 20:12:07 +0000

    added overlay for collection item

--- src/components/Collection/CollectionItem.tsx
+++ src/components/Collection/CollectionItem.tsx
@@ -1,3 +1,7 @@
+import { MdPlayArrow } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
 interface CollectionItemProps {
   poster: string;
   title: string;
@@ -5,11 +9,23 @@ interface CollectionItemProps {
 
 const CollectionItem: React.FC<CollectionItemProps> = ({ poster, title }) => {
   return (
-    <div className="w-full h-[240px] rounded-2xl overflow-hidden">
+    <motion.div className="w-full h-[240px] rounded-2xl overflow-hidden relative">
       <div className="w-full h-full relative">
         <img src={poster} alt={title} className="w-full h-full object-cover" />
       </div>
-    </div>
+
+      {/* Overlay */}
+      <motion.div className="absolute inset-0 bg-black/80 backdrop-blur-sm z-20 w-full h-full flex items-center justify-center">
+        <Link
+          to={`/collection/${title}`}
+          className="w-16 h-16 rounded-full flex items-center justify-center bg-primary text-white"
+        >
+          <motion.span>
+            <MdPlayArrow size={40} />
+          </motion.span>
+        </Link>
+      </motion.div>
+    </motion.div>
   );
 };
 export default CollectionItem;
